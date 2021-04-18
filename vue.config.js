366ed5eddc0a654f993b7ad1176e3a9c81db5cc6
masa-dev <mssa.massan@gmail.com>
2021-04-19 01:34:12 +0900

    基本となるtitleの設定

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const siteConfig = require('./config')
 
 module.exports = {
   publicPath: siteConfig.publicPath,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: siteConfig.title
+    }
+  },
 }
