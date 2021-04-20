8b61ea6c2206e30b27448f608319c441c3842880
masa-dev <mssa.massan@gmail.com>
2021-04-21 00:33:29 +0900

    シンタックスハイライタの導入

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -3,22 +3,3 @@
     <router-view />
   </div>
 </template>
-
-<style lang="scss">
-pre {
-  overflow-x: scroll;
-  margin: 15px;
-  padding: 10px;
-  font-size: 1.1rem;
-  font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier,
-    monospace;
-  border: 1px solid gray;
-  border-radius: 6px;
-}
-
-@media only screen and (max-width: 950px) {
-  pre {
-    margin: 0;
-  }
-}
-</style>
