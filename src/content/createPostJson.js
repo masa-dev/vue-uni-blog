2200f12c4928b0abcbe0c8fa11969c0fa8f0b645
masa-dev <mssa.massan@gmail.com>
2021-09-06 23:36:19 +0900

    feat: markedを用いてマークダウンの内容をhtmlに変換し、JSONファイルで出力する機能の追加
    今まで記事内容のmdファイルを、別のマークダウン変換ツールを用いて変換していたが、jsonファイルを介することによって、jsonファイルから読み込めるように変更。
    jsonを記事内容に反映させる機能はこれから追加する。

---
+++ src/content/createPostJson.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const marked = require("marked");
+const fs = require("fs");
+
+const dir = './src/content/post';
+const outDir = './src/content/export';
+const outFileName = 'content.json';
+const fileList = fs.readdirSync(dir);
+fileList.sort();
+
+let contentList = [];
+let contentListJSON;
+
+for (let fileName of fileList) {
+  let content = fs.readFileSync(`${dir}/${fileName}`, 'utf-8');
+  content = getMarkdownProperties(content, fileName);
+  let htmlMarked = {
+    property: content.property,
+    body: marked(content.body).replace(/<pre>/g, '<pre v-highlightjs>'),
+  }
+  contentList.push(htmlMarked);
+}
+
+contentListJSON = JSON.stringify(contentList,); // null, 2
+fs.writeFileSync(`${outDir}/${outFileName}`, contentListJSON, 'utf-8');
+
+
+// マークダウンのプロパティと内容を分割する関数
+function getMarkdownProperties(content = "", fileName = "") {
+  let startIndex = content.indexOf('---', 0);
+  let endIndex = content.indexOf('---', 1) + 3; // 最後の文字列も含める
+  let property = { fileName: fileName.replace(/\.md/g, '') };
+  let body = "";
+
+  if (startIndex === 0) {
+    let propStr = content.substring(startIndex + 3, endIndex - 3);
+    let propStrArray = propStr.split(/\r\n|\r|\n/);
+
+    for (let prop of propStrArray) {
+      if (prop !== '') {
+        let [propName, propContent] = prop.split(':');
+        property[propName] = propContent.trim();
+      }
+    }
+
+    body = content.substring(endIndex, content.length - 1).trim();
+  }
+
+  return {
+    property: property,
+    body: body,
+  };
+}
