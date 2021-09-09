e1513b1b7b1042e97040da5b4154fd836c316224
masa-dev <mssa.massan@gmail.com>
2021-09-10 05:09:31 +0900

    feat: Prettierを使用できるように変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    "@vue/prettier"
   ],
   parserOptions: {
     parser: 'babel-eslint'
