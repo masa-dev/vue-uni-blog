48ec0b079629b82beac277a2acae5dd8e8f97023
masa-dev <mssa.massan@gmail.com>
2021-09-10 03:28:46 +0900

    feat: 記事一覧のページを自動で生成するように変更

--- src/views/posts/PostHome.vue
+++ src/views/posts/PostHome.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
     <h2>記事</h2>
-    <p>必要があれば，ここに記録などをしていこうと考えています．</p>
     <ul>
-      <li><router-link to="/post/2021-07-06">URLでGoogleカレンダーに登録する</router-link></li>
-      <li><router-link to="/post/2021-06-28">ES2021とかで興味を持ったものとか</router-link></li>
-      <li><router-link to="/post/2021-06-15">分割代入</router-link></li>
-      <li><router-link to="/post/2021-05-18">非同期処理とコールバック</router-link></li>
-      <li><router-link to="/post/2021-05-11">JavaScriptで使われる構文など</router-link></li>
-      <li><router-link to="/post/2021-04-26">クロージャ</router-link></li>
-      <li><router-link to="/post/2021-04-12">javascriptのプライベートについて</router-link></li>
-      <li><router-link to="/post/2021-03-05">サイトの構成</router-link></li>
+      <li v-for="post in posts" :key="post.property.fileName">
+        <router-link :to="`/post/${post.property.fileName}`">{{
+          post.property.title
+        }}</router-link>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import content from "@/content/content.json";
+
 export default {
   title: "Post",
+  data() {
+    return {
+      posts: content.reverse(),
+    };
+  },
 };
 </script>
