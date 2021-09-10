6466072681c1c0f0dd14b25563aa59e4ac55dd59
masa-dev <mssa.massan@gmail.com>
2021-09-10 17:01:26 +0900

    feat: Not Found時にリクエストURLを表示するように変更

--- src/views/404.vue
+++ src/views/404.vue
@@ -2,11 +2,17 @@
   <div class="404">
     <h2>404 - Not Found</h2>
     <p>お探しのページは見つかりませんでした。</p>
+    <p>{{ url }}</p>
   </div>
 </template>
 
 <script>
 export default {
   title: "404 Not Found",
+  data() {
+    return {
+      url: `${location.origin}/#${this.$router.history.current.fullPath}`,
+    };
+  },
 };
 </script>
