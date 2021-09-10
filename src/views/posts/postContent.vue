6b0fc0721decde4d9bc1b27dc473b6539f63d6ca
masa-dev <mssa.massan@gmail.com>
2021-09-10 17:19:03 +0900

    fix: 関数が読み込めない問題の修正

--- src/views/posts/postContent.vue
+++ src/views/posts/postContent.vue
@@ -5,11 +5,37 @@
 <script>
 import content from "@/content/content.json";
 
+function getPost(id, callback) {
+  callback(false, searchPost(id).body);
+}
+
+function searchPost(id) {
+  for (let post of content) {
+    if (post.property.fileName === id) {
+      return {
+        body: post.body,
+        title: post.property.title,
+      };
+    }
+  }
+
+  let errHtml = `
+  <h2>404 - Not Found</h2>
+  <p>お探しのページは見つかりませんでした。</p>
+  <p>リクエストURL： ${location.origin}/#/post/${id}</p>
+  `;
+
+  return {
+    body: errHtml,
+    title: "404: Not Found",
+  };
+}
+
 export default {
   title: "",
   data() {
     return {
-      body: this.searchPost(this.$route.params.id).body,
+      body: searchPost(this.$route.params.id).body,
       id: this.$route.params.id,
     };
   },
@@ -21,44 +47,20 @@ export default {
         this.body = post;
       }
     },
-    getPost(id, callback) {
-      callback(false, this.searchPost(id).body);
-    },
-    searchPost(id) {
-      for (let post of content) {
-        if (post.property.fileName === id) {
-          return {
-            body: post.body,
-            title: post.property.title,
-          };
-        }
-      }
-
-      let errHtml = `
-        <h2>404 - Not Found</h2>
-        <p>お探しのページは見つかりませんでした。</p>
-        <p>リクエストURL： ${location.origin}/#/post/${id}</p>
-      `;
-
-      return {
-        body: errHtml,
-        title: "404: Not Found",
-      };
-    },
   },
   beforeMount() {
-    this.$options.title = this.searchPost(this.$route.params.id).title;
+    this.$options.title = searchPost(this.$route.params.id).title;
   },
   beforeRouteEnter(route, redirect, next) {
     redirect;
-    this.getPost(route.params.id, (err, post) => {
+    getPost(route.params.id, (err, post) => {
       next((vm) => vm.setData(err, post));
     });
   },
   beforeRouteUpdate(to, from, next) {
     from;
     this.body = null;
-    this.getPost(to.params.id, (err, post) => {
+    getPost(to.params.id, (err, post) => {
       this.setData(err, post);
       next();
     });
