94da4d2b33ab53bd645dc35bfffeb884e0f38ee3
masa-dev <mssa.massan@gmail.com>
2021-09-12 03:21:06 +0900

    feat: flickingのスライドをループするように変更

--- public/product/threePanelComic/sample2/js/flick.js
+++ public/product/threePanelComic/sample2/js/flick.js
@@ -1,6 +1,7 @@
 const flicking = new Flicking("#carousel", {
   align: "center",
-  circular: false,
+  circular: true,
+  deceleration: 0.05,
   bound: false,
   renderOnlyVisible: true,
 });
@@ -10,18 +11,12 @@ flicking.on("ready", (e) => {
 });
 
 flicking.on("changed", (e) => {
-  //document.getElementById("selected-index").innerHTML = `${e.index + 1} / ${e.currentTarget.panelCount}`;
-
-  // 選択中のスライドのクラス変更
-  //e.prevPanel._el.classList.remove("selected-panel");
-  //e.panel._el.classList.add("selected-panel");
-
   const emotionTexts = [
     "少しの不満",
     "すごい怒り",
     "何とかするべきだ",
     "変な人と思われたくない"
-  ]
+  ];
 
   document.getElementById("emotional-image").src = `img/sample-2-${e.index}.png`;
   document.getElementById("emotional-text").innerHTML = emotionTexts[e.index];
@@ -31,5 +26,5 @@ flicking.addPlugins(new Flicking.Plugins.Arrow());
 flicking.addPlugins(new Flicking.Plugins.Fade());
 flicking.addPlugins(new Flicking.Plugins.Pagination());
 
-flicking.activePlugins[1].scale = 0.6;
+flicking.activePlugins[1].scale = 0.3;
 flicking.activePlugins[1].update();
