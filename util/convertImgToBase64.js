968f6c6d8b0b20a36d98849e0b80edaf85d2d111
masa-dev <mssa.massan@gmail.com>
2021-09-16 05:42:08 +0900

    feat: base64画像の拡張子を自動で設定する機能の追加

--- util/convertImgToBase64.js
+++ util/convertImgToBase64.js
@@ -10,8 +10,20 @@ const imgToBase64 = require("./imgToBase64");
 function convertImgToBase64(html, imgFolderPath) {
   let convertedHtml = html.replace(/<img(\s)src=\".+?\"/g, function (match) {
     let url = match.match(/\"(?<url>.+)\"/).groups.url;
-    url = url.match(/[^\/]+$/);
-    let base64 = imgToBase64(`${imgFolderPath}/${url}`, "image/png");
+    url = url.match(/[^\/]+$/)[0];
+
+    let ext = url.match(/[^\.]+$/)[0];
+    ext = ext.toLowerCase();
+    let type;
+
+    if (ext === "jpg" || ext === "jpeg")
+      type = "image/jpeg";
+    else if (ext === "png")
+      type = "image/png";
+    else
+      type = "image/" + ext;
+
+    let base64 = imgToBase64(`${imgFolderPath}/${url}`, type);
 
     return `<img src="${base64}"`;
   })
